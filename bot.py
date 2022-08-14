0f1be872031f9d58301d58d1794a863db1148cd5
sashatsap <77918425+sashatsap@users.noreply.github.com>
2022-08-14 14:02:34 +0300

    make core for bot

--- bot.py
+++ bot.py
@@ -1,1 +1,30 @@
-print('hello world')
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+
+# Initialize bot and dispatcher
+bot = Bot(token=TELEGRAM_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+# Configure logging
+logging.basicConfig(
+    format=u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s',
+    level=logging.INFO,
+    # filename="bunker.log",
+)
+
+dp.middleware.setup(LoggingMiddleware())
+
+
+@dp.message_handler(commands=['start'])
+async def start_function(message: types.Message):
+    await message.answer('hello world')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
